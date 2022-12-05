2392ee5b84293844008263d7cfc0b9bb9589f1f8
Vyacheslav Pukhanov <vyacheslav@hey.com>
2022-12-05 17:06:44 +0300

    add simple split pane setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,33 @@
+import { ChangeEvent, useState } from 'react';
+import S from './index.module.css';
+
+const initialCodeString = `
+import React from 'react'
+
+export default function App() {
+ return (
+   <div>
+     <h1>Hello Playground</h1>
+     <h2>Start editing to see some magic happen!</h2>
+   </div>
+ )
+}
+`.trim();
+
 export default function Index() {
-  return <div></div>;
+  const [code, setCode] = useState(initialCodeString);
+
+  const onCodeChange = (ev: ChangeEvent<HTMLTextAreaElement>) =>
+    setCode(ev.target.value);
+
+  return (
+    <div className={S.grid}>
+      <textarea value={code} spellCheck={false} onChange={onCodeChange} />
+      <Preview />
+    </div>
+  );
+}
+
+function Preview() {
+  return <iframe src='/preview' />;
 }
