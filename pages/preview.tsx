be459d0ecda5eddc19a05c6cb1b4a2629597faa6
Vyacheslav Pukhanov <vyacheslav@hey.com>
2022-12-05 17:38:13 +0300

    compile code and render preview

--- pages/preview.tsx
+++ pages/preview.tsx
@@ -1,10 +1,14 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeJsonDecoder } from '@urlpack/json';
+import { executeCode } from '../lib/code-utils';
 
 const decoder = makeJsonDecoder();
 
 export default function Preview() {
   const [code, setCode] = useState('');
+  const [preview, setPreview] = useState<React.ReactNode>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     if (window.location.hash) {
@@ -14,5 +18,27 @@ export default function Preview() {
     }
   }, []);
 
-  return <div>{code}</div>;
+  useEffect(() => {
+    if (!code) {
+      return;
+    }
+
+    setError('');
+    setLoading(true);
+
+    executeCode(code, { react: React })
+      .then((defaultExport) => {
+        const CompiledPreview = defaultExport as React.ComponentType;
+        return setPreview(<CompiledPreview />);
+      })
+      .catch((error) => setError(error.toString()))
+      .finally(() => setLoading(false));
+  }, [code]);
+
+  return (
+    <>
+      {error}
+      {loading ? 'Loading preview...' : preview}
+    </>
+  );
 }
